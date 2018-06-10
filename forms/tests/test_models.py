ece299f63fa3884e80c90db9f1e70e6151daae88
Adam Sommer <adam@thehoick.com>
2018-06-10 09:26:58 -0400

    Created Input model and have basic CRUD tests.

--- forms/tests/test_models.py
+++ forms/tests/test_models.py
@@ -1,5 +1,9 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
-from forms.models import Form
+from forms.models import Form, Input
+
+
+User = get_user_model()
 
 
 class FormTestCase(TestCase):
@@ -25,11 +29,32 @@ class FormTestCase(TestCase):
         self.assertEqual(Form.objects.all().count(), 0)
 
 
+class InputTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(
+            username='test',
+            password='testers',
+            email='test@thehoick.com'
+        )
+        self.input = Input.objects.create(
+            status='new',
+            data='{"things": "yes please...", "more": true}',
+            user=self.user
+        )
 
+    def test_create(self):
+        self.assertEqual(self.input.status, 'new')
+        self.assertEqual(self.input.user.username, 'test')
 
+    def test_update(self):
+        self.input.status = 'routed'
+        self.input.save()
 
+        input = Input.objects.get(pk=self.input.pk)
 
+        self.assertEqual(input.status, 'routed')
 
-
-
-
+    def test_delete(self):
+        self.assertEqual(Input.objects.all().count(), 1)
+        self.input.delete()
+        self.assertEqual(Input.objects.all().count(), 0)
