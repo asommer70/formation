0ad38fcecdea95aeff7cd2e53232b4580c77dd6e
Adam Sommer <asommer70@gmail.com>
2018-07-13 06:52:07 -0400

    Setup input and buttons for sending Input to user and added check for sassc version to adjust the -m option.

--- components/sass_watcher.sh
+++ components/sass_watcher.sh
@@ -5,6 +5,13 @@
 fswatch -0 scss/ | while IFS= read -r -d "" path
 echo $path
 do
-  echo "Re-building CSS (due to change in ${path})"
-  /usr/local/bin/sassc -m auto -t compressed scss/main.scss ../assets/css/main.css
+    echo "Re-building CSS (due to change in ${path})"
+
+    # Check the version of sassc (Ubuntu 18.04 uses 3.4.5 which doesn't require an argument after the -m).
+    version=$(sassc -v | grep sassc)
+    if [[ $versin -eq "sassc: 3.4.5" ]]; then
+       /usr/local/bin/sassc -m -t compressed scss/main.scss ../assets/css/main.css
+    else
+       /usr/local/bin/sassc -m auto -t compressed scss/main.scss ../assets/css/main.css
+    fi
 done
