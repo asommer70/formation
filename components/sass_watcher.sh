9582b45a1ce67825282dbd4464c78d87de4cf851
Adam Sommer <asommer70@gmail.com>
2018-07-26 16:50:17 -0400

    Styled Foundation with some custom colors... not sure I'm 100% liking them, but good enough for now.  Also, working on adding a favicon.

--- components/sass_watcher.sh
+++ components/sass_watcher.sh
@@ -9,7 +9,7 @@ do
 
     # Check the version of sassc (Ubuntu 18.04 uses 3.4.5 which doesn't require an argument after the -m).
     version=$(sassc -v | grep sassc)
-    if [[ $versin -eq "sassc: 3.4.5" ]]; then
+    if [[ "$version" = "sassc: 3.4.5" ]]; then
        /usr/local/bin/sassc -m -t compressed scss/main.scss ../assets/css/main.css
     else
        /usr/local/bin/sassc -m auto -t compressed scss/main.scss ../assets/css/main.css
